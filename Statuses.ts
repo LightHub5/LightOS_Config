581bae98ec6a01bf952e1b61666474db70f2aed5
Dmitry Levshin <65412163+devbouf@users.noreply.github.com>
2023-05-28 03:36:04 +0300

    Create Statuses.ts

---
+++ Statuses.ts
@@ -0,0 +1,10 @@
+import {ActivityType} from "discord.js";
+export const statuses = {
+    changeTime: 5000,
+    status: [
+        {
+            name: 'Боуф лучший!',
+            type: ActivityType.Watching
+        },
+        ]
+}

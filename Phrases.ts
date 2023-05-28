e7b45a60de9e5f5c5dac3f61308e78a14b4748f5
stefanlight <steffmukhin@gmail.com>
2023-05-28 20:54:15 +0300

    Update Phrases.ts

--- Phrases.ts
+++ Phrases.ts
@@ -3,5 +3,10 @@ export const phrases = [
         phrase: 'Это не баг, это фича',
         authorId: '1086308186644021309',
         author: 'Wayment'
+    },
+    {
+        phrase: '- Пора бы LightHub делать, - предложил Дима.',
+        authorId: '890784413242241084',
+        author: 'stefanlight'
     }
 ]
